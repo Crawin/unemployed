67ae6a34cc967f0b3150fe1422a4b8a0484b1a02
Son Jungwon <sonjw3000@gmail.com>
2024-01-16 20:47:52 +0900

    임시

---
+++ BLAH/BlahBlah/COOL/Include/ECS/Entity.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+class Component;
+
+class Entity
+{
+	std::unordered_map<int, Component*> m_Components;
+
+public:
+
+	template <class T>
+	void AddComponent() {
+		int compID = T::GetIndex();
+		if (m_Components.contains(compID)) {
+			// 이미 있다
+			return;
+		}
+
+
+		T* component = new T;
+
+	}
+};
