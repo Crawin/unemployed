4d7ef8463b7d376020a9dbf2374d50f15e77dd6c
Lee Seung Hak <99tmdgkr@gmail.com>
2024-03-11 19:53:24 +0900

    아이피를 도메인으로 설정

--- BLAH/BlahBlah/COOL/Include/Network/Client.cpp
+++ BLAH/BlahBlah/COOL/Include/Network/Client.cpp
@@ -4,8 +4,8 @@
 
 Client::Client()
 {
-	//m_cpServerIP = (char*)"freerain.mooo.com";
-	m_cpServerIP = (char*)"127.0.0.1";
+	m_cpServerIP = (char*)"freerain.mooo.com";
+	//m_cpServerIP = (char*)"127.0.0.1";
 	m_Sock = NULL;
 }
 
@@ -33,23 +33,16 @@ int Client::Connect_Server()
 	m_Sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (m_Sock == INVALID_SOCKET) err_quit("socket()");
 
-
-
 	// connect()
 	struct sockaddr_in serveraddr;
 	memset(&serveraddr, 0, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
-	if (strncmp(m_cpServerIP, "127", 3)) {		// 아이피가 루프백 주소라면
-		inet_pton(AF_INET, m_cpServerIP, &serveraddr.sin_addr);
+	if (strncmp(m_cpServerIP, "free", 4) == 0) {			// 아이피가 도메인이면
+		struct hostent* ptr = gethostbyname(m_cpServerIP);
+		memcpy(&serveraddr.sin_addr, ptr->h_addr, ptr->h_length);
 	}
 	else {
-		struct hostent* ptr = gethostbyname(m_cpServerIP);
-		struct in_addr addr;
-		memcpy(&addr, ptr->h_addr, ptr->h_length);
-		//char IP[INET_ADDRSTRLEN];
-		//inet_ntop(AF_INET, &addr, IP, sizeof(IP));
-		//inet_pton(AF_INET, IP, &serveraddr.sin_addr);
-		serveraddr.sin_addr = addr;
+		inet_pton(AF_INET, m_cpServerIP, &serveraddr.sin_addr);
 	}
 	serveraddr.sin_port = htons(SERVERPORT);
 	retval = connect(m_Sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
