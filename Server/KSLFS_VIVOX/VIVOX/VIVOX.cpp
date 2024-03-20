a799f54143ad0f1e70714f18fb646d23a0d725f3
Lee Seung Hak <99tmdgkr@gmail.com>
2024-03-20 21:06:53 +0900

    logout, 통화방 이탈 추가

--- Server/KSLFS_VIVOX/VIVOX/VIVOX.cpp
+++ Server/KSLFS_VIVOX/VIVOX/VIVOX.cpp
@@ -18,6 +18,8 @@ void Start_Vivox()
         return;
     }
     defaultConfig.pf_sdk_message_callback = &OnResponseOrEventFromSdk;
+    defaultConfig.pf_on_audio_unit_before_recv_audio_mixed = &OnBeforeReceivedAudioMixed;
+    //defaultConfig.pf_on_audio_unit_before_recv_audio_rendered = &OnAudioBeforeRecvAudioRendered;
     status = vx_initialize3(&defaultConfig, sizeof(defaultConfig));
 
     if (status != VxErrorSuccess) {
@@ -45,6 +47,15 @@ void Start_Vivox()
             std::cout << "방 번호를 입력하시오." << std::endl;
             std::cin >> gameNum;
             RequestParticipate(sockNum,gameNum);
+        }
+        else if (command == "exit")
+        {
+            ExitChannel(gameNum);
+        }
+        else if (command == "logout")
+        {
+            LogOut(sockNum);
+            break;
         }
     }
     // 프로그램 종료 전 uninitialize
@@ -82,6 +93,9 @@ void MyGamesEventHandler(vx_evt_base_t* evt)
     case evt_session_added:
         HandleSessionAdded(reinterpret_cast<vx_evt_session_added*>(evt));
         break;
+    default:
+        std::cout << "적용되지 않은 이벤트 콜백" << std::endl;
+        break;
     }
 }
 
@@ -97,6 +111,11 @@ void MyGamesResponseHandler(vx_resp_base_t* resp)
         break;
     case resp_sessiongroup_add_session:
         HandleSessiongroupAddSession(reinterpret_cast<vx_resp_sessiongroup_add_session*>(resp));
+        break;
+    case resp_sessiongroup_remove_session:
+        break;
+    default:
+        std::cout << "적용되지 않은 리스폰스 콜백" << std::endl;
         break;
     }
 }
@@ -126,6 +145,52 @@ void OnResponseOrEventFromSdk(void* callback_handle)
             break;
         }
     }
+}
+
+void OnAudioBeforeRecvAudioRendered(
+    void* callback_handle,
+    const char* session_group_handle,
+    const char* initial_target_uri,
+    short* pcm_frames,      // 프레임 버퍼
+    int pcm_frame_count,    // 버퍼 내 채널 수
+    int audio_frame_rate,   // 샘플 레이트, 스트림 도중 전환 가능
+    int channels_per_frame, // 프레임당 채널
+    int is_silence)          // 렌더 가능 오디오 데이터가 있으면 0
+{
+    std::cout << "오디오 콜백 등장" << std::endl;
+}
+
+void OnBeforeReceivedAudioMixed(
+    void* callback_handle,
+    const char* session_group_handle,
+    const char* initial_target_uri,
+    vx_before_recv_audio_mixed_participant_data_t* participants_data,
+    size_t num_participants)
+{
+    std::cout << "오디오 콜백 등장" << std::endl;
+
+    //std::string example_uri = "example_uri";
+    //// 다수의 오디오 스트림에 걸쳐 반복
+    //for (unsigned int i = 0; i < num_participants; i++) {
+    //    auto& data = participants_data[i];
+
+    //    int channels_per_frame = data.channels_per_frame;
+    //    std::string participant_uri = data.participant_uri;
+
+    //    //if (participant_uri == example_uri) {
+    //        //for (unsigned int f = 0; f < data.pcm_frame_count; f++) {
+    //        //    for (unsigned int c = 0; c < channels_per_frame; c++) {
+    //        //        // 오디오 데이터 사용/수정. 여기서는 단순히 샘플을 2로 나눕니다.
+    //        //        data.pcm_frames[(f * channels_per_frame) + c] /= 10;
+    //        //    }
+    //        //}
+    //    //}
+    //    auto tempdata = data.pcm_frames;    // 볼륨값
+    //    for (unsigned int f = 0; f < data.pcm_frame_count/4; ++f)
+    //    {
+    //        data.pcm_frames[f] = (tempdata[f*4]+tempdata[f*4+1]+ tempdata[f * 4+2] + tempdata[f * 4 + 3]) / 4;
+    //    }
+    //}
 }
 
 void CreateConnector(vx_req_connector_create*& req)
@@ -193,14 +258,14 @@ void HandleLoginStateChange(vx_evt_account_login_state_change* evt)
 void RequestParticipate(const int& sock, const int& gameNum)
 {
     std::string sock_str = std::to_string(sock);
-
-    //char* temp = vx_get_echo_channel_uri("echotest", VIVOX_DOMAIN, VIVOX_ISSUER);       // 에코채널
-    char* temp = vx_get_general_channel_uri(&std::to_string(gameNum)[0], VIVOX_DOMAIN, VIVOX_ISSUER);       // 논포지셔널채널
+    std::string gameNum_str = std::to_string(gameNum);
+    char* temp = vx_get_echo_channel_uri(&gameNum_str[0], VIVOX_DOMAIN, VIVOX_ISSUER);       // 에코채널
+    //char* temp = vx_get_general_channel_uri(&std::to_string(gameNum)[0], VIVOX_DOMAIN, VIVOX_ISSUER);       // 논포지셔널채널
 
     vx_req_sessiongroup_add_session* req;
     vx_req_sessiongroup_add_session_create(&req);
-    req->sessiongroup_handle = vx_strdup(&sock_str[0]);
-    req->session_handle = vx_strdup(temp);
+    req->sessiongroup_handle = vx_strdup(&gameNum_str[0]);
+    req->session_handle = vx_strdup(&std::string("Game" + gameNum_str)[0]);
     req->uri = vx_strdup(temp);
     req->account_handle = vx_strdup(&std::string("SOCK_" + sock_str)[0]);
     req->connect_audio = 1;
@@ -213,7 +278,7 @@ char* GenerateToken(const char* payload, const char* uri,const int& sock)
     //int socketnum = 100;
     std::string UserName = VIVOX_ISSUER;
     UserName += '.' + std::to_string(sock);
-    char* hi = vx_get_user_uri(&UserName[0], VIVOX_DOMAIN, NULL);
+    //char* hi = vx_get_user_uri(&UserName[0], VIVOX_DOMAIN, NULL);
 
     return vx_strdup(vx_debug_generate_token(VIVOX_ISSUER, vx_time_t(-1), payload, 2, NULL, vx_get_user_uri(&UserName[0], VIVOX_DOMAIN, NULL), uri, (const unsigned char*)VIVOX_KEY, strlen(VIVOX_KEY)));
 }
@@ -297,4 +362,23 @@ void HandleSessiongroupAdded(vx_evt_sessiongroup_added* evt)
 void HandleSessionAdded(vx_evt_session_added* evt)
 {
     std::cout << "[" << evt->session_handle << "] SessionAdded" << std::endl;
+}
+
+void ExitChannel(const int& gameNum)
+{
+    std::string gameNum_str = std::to_string(gameNum);
+    vx_req_sessiongroup_remove_session* req;
+    vx_req_sessiongroup_remove_session_create(&req);
+    req->sessiongroup_handle = vx_strdup(&gameNum_str[0]);
+    req->session_handle = vx_strdup(&std::string("Game" + gameNum_str)[0]);
+    vx_issue_request3(&req->base, NULL);
+}
+
+void LogOut(const int& sock)
+{
+    std::string sock_str = std::to_string(sock);
+    vx_req_account_logout* req;
+    vx_req_account_logout_create(&req);
+    req->account_handle = vx_strdup(&std::string("SOCK_" + sock_str)[0]);
+    vx_issue_request3(&req->base, NULL);
 }
