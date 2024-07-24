b983fc19fa629e28693879dd5e0e79b9db40e4ac
Son Jungwon <sonjw3000@gmail.com>
2024-07-25 05:17:48 +0900

    아웃라인과 분리

--- BLAH/BlahBlah/COOL/Include/Shader/Shader.cpp
+++ BLAH/BlahBlah/COOL/Include/Shader/Shader.cpp
@@ -147,6 +147,11 @@ D3D12_RASTERIZER_DESC Shader::GetRasterizerStateDesc(int presetID)
 	case 1:
 		// cull front
 		// shadowmap
+		desc.CullMode = D3D12_CULL_MODE_FRONT;
+		break;
+	case 2:
+		// cull front
+		// shadowmap
 		desc.CullMode = D3D12_CULL_MODE_NONE;
 		break;
 	default:
